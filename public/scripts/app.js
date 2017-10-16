010adaaeb509ca67eac50e55cb2e8d9858b64c2a
Diegous <duribe@cobralex.cl>
2017-10-16 15:56:32 -0300

    configuration and testing

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var square = function square(x) {
+  return x * x;
+};
+
+var squareArrow = function squareArrow(x) {
+  return x * x;
+};
+
+console.log(square(8));
+console.log(squareArrow(8));
